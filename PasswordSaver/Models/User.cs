4a8ce9c5f14c8bf67ade22805d53c349ebebe7de
Balychev Kiril <mister.balychev@gmail.com>
2024-08-12 16:39:41 +0300

    Created user class

---
+++ PasswordSaver/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace PasswordSaver.Models
+{
+    public class User
+    {
+        public User(Guid id, string userName, string passwordHash, string email)
+        {
+            Id = id;
+            UserName = userName;
+            PasswordHash = passwordHash;
+            Email = email;
+        }
+
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+
+        public static User Create(Guid id, string userName, string passwordHash, string email)
+        {
+            return new User(id, userName, passwordHash, email);
+        }
+    }
+}
