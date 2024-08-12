2cb4cf75b060c0d740ca44136eb3338d845336a2
Balychev Kiril <mister.balychev@gmail.com>
2024-08-12 16:24:59 +0300

    Добавьте файлы проекта.

---
+++ PasswordSaver/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PasswordSaver.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
