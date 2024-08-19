1c97077097341a64fc5d371a4961ea32e319a948
Balychev Kiril <mister.balychev@gmail.com>
2024-08-19 19:10:22 +0300

    Updated role configuration

--- PasswordSaver/Configurations/RoleConfiguration.cs
+++ PasswordSaver/Configurations/RoleConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using PasswordSaver.Entities;
+using PasswordSaver.Enums;
 
 namespace PasswordSaver.Configurations
 {
@@ -15,6 +16,16 @@ namespace PasswordSaver.Configurations
                 .UsingEntity<RolePermissionEntity>(
                     l => l.HasOne<PermissionEntity>().WithMany().HasForeignKey(e => e.PermissionId),
                     r => r.HasOne<RoleEntity>().WithMany().HasForeignKey(e => e.RoleId));
+
+            var roles = Enum
+                .GetValues<Role>()
+                .Select(r => new RoleEntity
+                {
+                    Id = (int)r,
+                    Name = r.ToString()
+                });
+
+            builder.HasData(roles);
         }
     }
 }
