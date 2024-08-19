9e0d1b7dc2549e45db655a4bb69a5128ba63adb5
Balychev Kiril <mister.balychev@gmail.com>
2024-08-19 19:07:03 +0300

    Added new RolePermission entity and Role configuration

---
+++ PasswordSaver/Configurations/RoleConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PasswordSaver.Entities;
+
+namespace PasswordSaver.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<RoleEntity>
+    {
+        public void Configure(EntityTypeBuilder<RoleEntity> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder.HasMany(r => r.Permissions)
+                .WithMany(r => r.Roles)
+                .UsingEntity<RolePermissionEntity>(
+                    l => l.HasOne<PermissionEntity>().WithMany().HasForeignKey(e => e.PermissionId),
+                    r => r.HasOne<RoleEntity>().WithMany().HasForeignKey(e => e.RoleId));
+        }
+    }
+}
